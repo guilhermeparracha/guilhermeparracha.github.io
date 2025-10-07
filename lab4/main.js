07152f77887b2e390eda6ced33524ada866600e6
guilhermeparracha <a22402027@alunos.ulht.pt>
2025-10-07 17:38:11 +0000

    lab4

---
+++ lab4/main.js
@@ -0,0 +1,28 @@
+let contador = 0;
+
+
+function aumentarContador() {
+    
+  contador++;
+  document.getElementById("contador").textContent = contador;
+}
+
+function mudarCorFundo() {
+  document.body.style.backgroundColor = "#d6eaf8";
+}
+
+function voltarCorFundo() {
+  document.body.style.backgroundColor = "#e8dfd0";
+}
+
+function mudarTexto() {
+  document.getElementById("mensagem").textContent = "Budapeste é uma cidade incrível!";
+}
+
+function aumentarImagem() {
+  document.querySelector("img").style.transform = "scale(1.1)";
+}
+
+function diminuirImagem() {
+  document.querySelector("img").style.transform = "scale(1)";
+}
